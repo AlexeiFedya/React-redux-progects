401e1f8251735ba6051a7579fce8ab567e092571
AlexeiFedya <57766198+AlexeiFedya@users.noreply.github.com>
2020-04-22 15:00:32 +0300

    refactoring reducer

--- redux-store/src/reducers/reducer.js
+++ redux-store/src/reducers/reducer.js
@@ -6,6 +6,51 @@ const initialState = {
     orderTotal: 220
 }
 
+const updateCartItems = (cartItems, item, idx) => {
+    if (idx === -1){
+        return [
+            ...cartItems,
+            item
+        ]
+    }
+
+    return [
+        ...cartItems.slice(0,idx),
+        item,
+        ...cartItems.slice(idx + 1)
+    ]
+}
+
+const updateCartItem = (book, item = {}) => {
+
+    const {id = book.id, 
+           count = 0,
+           title = book.title,
+           total = 0} = item;
+           
+            return {
+                id,
+                title,
+                count: count + 1,
+                total: total + book.price
+            }
+
+            // if (item) {
+            //     return {
+            //         ...item,
+            //         count: item.count + 1,
+            //         total: item.total + book.price
+            //     }
+            // } else {
+            //     return {
+            //         id: book.id,
+            //         title: book.title,
+            //         count: 1,
+            //         total: book.price
+            //     }
+            // }
+}
+
 const reducer = (state = initialState, action) => {
 
     console.log(action.type)
@@ -40,41 +85,13 @@ const reducer = (state = initialState, action) => {
             const itemIndex = state.cartItems.findIndex((book) => book.id === bookId)
             const item = state.cartItems[itemIndex]
             
-            let newItem;
-
-            if (item) {
-                newItem = {
-                    ...item,
-                    count: item.count + 1,
-                    total: item.total + book.price
-                }
-            } else {
-                newItem = {
-                    id: book.id,
-                    title: book.title,
-                    count: 1,
-                    total: book.price
-                }
-            }
+            const newItem =  updateCartItem (book, item)
             
-            if(itemIndex < 0){
                 return {
                     ...state,
-                    cartItems: [
-                        ...state.cartItems,
-                        newItem
-                    ]
+                    cartItems: updateCartItems(state.cartItems, newItem, itemIndex)
                 }
-            } else {
-                return {
-                    ...state,
-                    cartItems: [
-                        ...state.cartItems.slice(0, itemIndex),
-                        newItem,
-                        ...state.cartItems.slice(itemIndex + 1)
-                    ]
-                }
-            }
+            
         
 
         default:
