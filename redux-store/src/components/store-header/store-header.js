a27c4fbccbe633a2f92a996d5f3f16cac10c0c37
AlexeiFedya <57766198+AlexeiFedya@users.noreply.github.com>
2020-05-18 17:08:24 +0300

    .

--- redux-store/src/components/store-header/store-header.js
+++ redux-store/src/components/store-header/store-header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './store-header.css'
 import { Link } from 'react-router-dom'
+import {connect} from 'react-redux'
 
-
-const StoreHeader = ({numItems, total}) => {
+const StoreHeader = ({items}) => {
     return (
     <header className="shop-header row">
         <Link to="/">
@@ -12,13 +12,21 @@ const StoreHeader = ({numItems, total}) => {
         <Link to="/cart">
             <div className="shopping-cart">
                 <i className="cart-icon fa fa-shopping-cart" />
-                {numItems} items (${total})
+                    {items?.reduce((prev, cur) =>  prev+cur.count,0)}
+                <span> items</span> 
+                (${items?.reduce((prev, cur) =>  prev+cur.total,0)})
             </div>
         </Link>
     </header>
     )
 }
 
+const mapStateToProps = ({shoppingCart}) => {
+    return {
+        items: shoppingCart.cartItems
+    }
+}
 
 
-export default StoreHeader;
+
+export default connect(mapStateToProps)(StoreHeader);
